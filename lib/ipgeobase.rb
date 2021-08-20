47ae2925be485241a9be18d7bc1f153e4b49a979
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-08-20 08:08:09 +0300

    create lookup method

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
+require "addressable/uri"
+require "net/http"
+require "happymapper"
 
+# module get metadata by IP address
 module Ipgeobase
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.lookup(ip)
+    uri = Addressable::URI.parse("http://ip-api.com/xml/#{ip}")
+    response = Net::HTTP.get(uri)
+    HappyMapper.parse(response)
+  end
 end
